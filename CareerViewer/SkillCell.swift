026169ed3ad732fd74e306ea05799ba226a31d89
saeedeh <saeedeh@tiseno.com>
2018-03-25 19:04:00 +0800

    SkillCell UI added

---
+++ CareerViewer/SkillCell.swift
@@ -0,0 +1,27 @@
+//
+//  SkillCell.swift
+//  CareerViewer
+//
+//  Created by Saeedeh on 25/03/2018.
+//  Copyright © 2018 tiseno. All rights reserved.
+//
+
+import UIKit
+
+class SkillCell: UITableViewCell {
+    
+    @IBOutlet var skillRateSlider: UISlider!
+    @IBOutlet var titleLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
