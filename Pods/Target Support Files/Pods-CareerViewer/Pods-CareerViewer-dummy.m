e9ca892577efbd9fb8e92a6d14e1a12312b1f9fa
saeedeh <saeedeh@tiseno.com>
2018-03-26 01:26:17 +0800

    commit pod file and image folders

---
+++ Pods/Target Support Files/Pods-CareerViewer/Pods-CareerViewer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CareerViewer : NSObject
+@end
+@implementation PodsDummy_Pods_CareerViewer
+@end
---
+++ Pods/Target Support Files/Pods-CareerViewerTests/Pods-CareerViewerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CareerViewerTests : NSObject
+@end
+@implementation PodsDummy_Pods_CareerViewerTests
+@end
