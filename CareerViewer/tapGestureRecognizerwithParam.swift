1baf22577ac972f57693f45b5b208f868d6b11f5
saeedeh <saeedeh@tiseno.com>
2018-03-25 19:56:55 +0800

    tapGestureRecognizerwithParam added

---
+++ CareerViewer/tapGestureRecognizerwithParam.swift
@@ -0,0 +1,14 @@
+//
+//  tapGestureRecognizerwithParam.swift
+//  CareerViewer
+//
+//  Created by Saeedeh on 25/03/2018.
+//  Copyright © 2018 tiseno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class tapGestureRecognizerwithParam: UITapGestureRecognizer {
+    var param: AnyObject?
+}
