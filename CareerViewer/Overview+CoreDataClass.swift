2d35c857d08ff6ad535ce9cfa5589166edf7530f
saeedeh <saeedeh@tiseno.com>
2018-03-25 14:16:05 +0800

    coredata for overview was added

---
+++ CareerViewer/Overview+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Overview+CoreDataClass.swift
+//  CareerViewer
+//
+//  Created by Saeedeh on 25/03/2018.
+//  Copyright © 2018 tiseno. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Overview)
+public class Overview: NSManagedObject {
+
+}
---
+++ CareerViewer/Overview+CoreDataProperties.swift
@@ -0,0 +1,24 @@
+//
+//  Overview+CoreDataProperties.swift
+//  CareerViewer
+//
+//  Created by Saeedeh on 25/03/2018.
+//  Copyright © 2018 tiseno. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Overview {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Overview> {
+        return NSFetchRequest<Overview>(entityName: "Overview");
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var descryption: String?
+    @NSManaged public var image: NSObject?
+    @NSManaged public var expirtTime: NSDate?
+
+}
