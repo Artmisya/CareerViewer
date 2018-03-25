9ea1500ea0122e90f1610bb408a19c9ce91463ba
saeedeh <saeedeh@tiseno.com>
2018-03-25 14:29:57 +0800

    contactTypes defined

---
+++ CareerViewer/ContactType.swift
@@ -0,0 +1,18 @@
+//
+//  ContactType.swift
+//  CareerViewer
+//
+//  Created by Saeedeh on 25/03/2018.
+//  Copyright © 2018 tiseno. All rights reserved.
+//
+
+import Foundation
+
+enum ContactType: String {
+    case email
+    case mobile
+    case website
+    case linkedin
+    case github
+    
+}
