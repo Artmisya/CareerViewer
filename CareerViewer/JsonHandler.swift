31ba9ba536b2c4a86ba13d6e5a9a2de32183e353
saeedeh <saeedeh@tiseno.com>
2018-06-24 20:47:38 +0800

    update for swift 4 and update the method to use Result enum

--- CareerViewer/JsonHandler.swift
+++ CareerViewer/JsonHandler.swift
@@ -10,26 +10,17 @@ import Foundation
 
 
 
-struct JsonResult {
-    
-    let data: [String:Any]?  // data will be nil in case an error happen during reading the file
-    let error: ErrorType?  // error will be nil in case the json file cen be read successfully
-    
-}
-
 class JsonHandler{
     
     //this method read a json file and returns the file content in a JsonResult format
     
-    class func readJsonFile(fileName:String)->JsonResult{
+    class func readJsonFile(fileName:String)->Result<Any>{
         
         if let path = Bundle.main.path(forResource: fileName, ofType: "json") {
             
             do {
                 let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
-                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves) as! [String:Any]
-                
-                let result=JsonResult(data: jsonResult, error: nil)
+                let result=Result.success(data as Any)
                 return result
                 
             } catch let error as NSError {
@@ -37,14 +28,14 @@ class JsonHandler{
                 print (error.localizedDescription)
                 
                 let error = ErrorType.generalError(message: error.localizedDescription)
-                let result=JsonResult(data: nil, error: error)
+                let result=Result<Any>.failure(error)
                 return result
             }
         }
         else{
             
             let error = ErrorType.generalError(message: ErrorMessage.failToOpenFile.rawValue)
-            let result=JsonResult(data: nil, error: error)
+            let result=Result<Any>.failure(error)
             return result
             
         }
@@ -53,3 +44,4 @@ class JsonHandler{
     
     
 }
+
