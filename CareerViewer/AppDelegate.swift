72347ecc56117591089b737d25e95f5b7ac8b73a
saeedeh <saeedeh@tiseno.com>
2018-06-24 20:34:37 +0800

    update to swift 4

--- CareerViewer/AppDelegate.swift
+++ CareerViewer/AppDelegate.swift
@@ -9,89 +9,59 @@
 import UIKit
 import CoreData
 
+import CoreData
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-    let coreDataExpirtTime=10
-    static let resume:Resume=Resume()
-
+    
+    let coreDataExpiryTime=10
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
+        // Override point for customization after application launch.
         settingAppUi()
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-     
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
- 
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-   
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
     
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+    
     func applicationWillTerminate(_ application: UIApplication) {
-
-        self.saveContext()
-    }
-
-    // MARK: - Core Data stack
-
-    lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-        */
-        let container = NSPersistentContainer(name: "CareerViewer")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-       
-                //fatalError("Unresolved error \(error), \(error.userInfo)")
-                print("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-
-    // MARK: - Core Data Saving support
-
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-              
-                let nserror = error as NSError
-                //fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-                print("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        // Saves changes in the application's managed object context before the application terminates.
+        let coreDataHandler=CoreDataHandler()
+        coreDataHandler.saveContext()
     }
     
     private func settingAppUi(){
         
         // setting the selected tabbar image and text color
         
-        UITabBar.appearance().tintColor = UIColor(colorLiteralRed: 155/225, green: 51/255, blue: 255/255, alpha: 1.0)
+        UITabBar.appearance().tintColor = UIColor(red: 155/225, green: 51/255, blue: 255/255, alpha: 1.0)
         
         //setting navigation bar and text color
         UINavigationBar.appearance().barTintColor = UIColor.white
         
-        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName:UIColor.black]
+        UINavigationBar.appearance().titleTextAttributes = [kCTForegroundColorAttributeName:UIColor.black] as [NSAttributedStringKey : Any]
         
     }
-
+    
+    
 }
 
